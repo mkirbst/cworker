7b349a38637759a00cee98d5fc7275dd0bbb8da7
marcel <mkirbst@htwm.de>
2011-11-15 17:07:58 +0100

    fileattributes passing to mysql database now elementary working, no error checking

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,23 +16,90 @@ using MySql.Data.MySqlClient;
 namespace worker  {
     class Program    {
         static void Main(string[] args)   {
+            
             //walkFolders("q:\\");
-            //walkFolders("C:\\Users\\kirbst\\Desktop\\buecher");
+            //walkFolders("C:\\Users\\kirbst\\Desktop\\buecher\\hardware\\apple");
+
+            sql_trunc_table();
+
+            walkFoldersSql("Q:\\");
+//            walkFoldersSql("C:\\Users\\kirbst\\Desktop\\buecher\\hardware\\apple");
 
             
-            sql_dump();
+
+            //sql_insert();
+            //sql_dump();
+            //
         }
 
-        public static bool sql_dump()
+        //INSERT INTO `test`.`worker` (`name`, `path`, `loc`, `size`, `csum`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('testdatei.pdf', 'c:\\testdateien\\testdatei.pdf', 'c:', '1234', '1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f530286 0c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75', 'm', 'm', CURRENT_TIMESTAMP, '1983-10-10 22:11:02', '1983-01-01 00:11:02', '1983-10-10 22:11:11', '1');
+
+
+        public static bool sql_insert()
         {
             bool erg = true;
 
             string myConnectionString = "SERVER=localhost;" +
                                         "DATABASE=test;" +
                                         "UID=admin;" +
-                                        "PASSWORD=cNtN.5db6!;";
+                                        "PASSWORD=password;";
 
-            Console.WriteLine("Versuche Verbindung zu: " + myConnectionString);
+            Console.WriteLine("sql_insert: Versuche Verbindung zu: " + myConnectionString);
+
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+
+
+
+            command.CommandText = "INSERT INTO `test`.`worker` (`name`, `path`, `loc`, `size`, `csum`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + "" + "', 'c:\\testdateien\\mmmm.pdf', 'c:', '1234', 'mmmmfc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75', 'm', 'm', CURRENT_TIMESTAMP, '1983-10-10 22:11:02', '1983-01-01 00:11:02', '1983-10-10 22:11:11', '1');";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            while (Reader.Read())
+            {
+                string row = "";
+                for (int i = 0; i < Reader.FieldCount; i++)
+                    row += Reader.GetValue(i).ToString() + ", ";
+
+                Console.WriteLine(row);
+            }
+            connection.Close();
+
+
+            Console.WriteLine("Trenne Verbindung...");
+            return erg;
+        }
+
+        public static bool sql_trunc_table()
+        {
+            bool erg = true;
+            string myConnectionString = "SERVER=localhost;" +
+                                        "DATABASE=test;" +
+                                        "UID=admin;" +
+                                        "PASSWORD=password;";
+
+            Console.WriteLine("sql_trunc_table: Versuche Verbindung zu: " + myConnectionString);
+            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "TRUNCATE TABLE worker;";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+            connection.Close();
+            Console.WriteLine("sql_trunc_table: Trenne Verbindung...");
+            return erg;
+        }
+
+        public static bool sql_dump()
+        {
+            bool erg = true;
+            int counter = 0;
+            string myConnectionString = "SERVER=localhost;" +
+                                        "DATABASE=test;" +
+                                        "UID=admin;" +
+                                        "PASSWORD=password;";
+
+            Console.WriteLine("sql_dump: Versuche Verbindung zu: " + myConnectionString);
             
             MySqlConnection connection = new MySqlConnection(myConnectionString);
             MySqlCommand command = connection.CreateCommand();
@@ -44,15 +111,78 @@ namespace worker  {
                 string row = "";
                 for (int i = 0; i < Reader.FieldCount; i++)
                     row += Reader.GetValue(i).ToString() + ", ";
+                
                 Console.WriteLine(row);
             }
             connection.Close();
 
 
-            Console.WriteLine("Trenne Verbindung...");
+            Console.WriteLine("sql_dump: Trenne Verbindung...");
             return erg;
         }
 
+
+
+        private static void walkFoldersSql(string DirectorySql)
+        {
+            walkFoldersSql(new DirectoryInfo(DirectorySql));
+        }
+
+        private static void walkFoldersSql(DirectoryInfo disql)
+        {
+
+            string sqlcommand = "";
+            int counter = 0;
+            string myConnectionString = "SERVER=localhost;" +
+                                        "DATABASE=test;" +
+                                        "UID=admin;" +
+                                        "PASSWORD=password;";
+
+            Console.WriteLine("walkFoldersSql: Versuche Verbindung ...");
+
+            try
+            {
+                // Alle Verzeichnisse rekursiv durchlaufen
+                foreach (DirectoryInfo subdir in disql.GetDirectories())
+                {
+                    walkFoldersSql(subdir);
+                }
+
+                // Alle Dateien durchlaufen
+                foreach (FileInfo fi in disql.GetFiles())
+                {
+                    MySqlConnection connection = new MySqlConnection(myConnectionString);
+                    MySqlCommand command = connection.CreateCommand();
+                    Console.WriteLine("walkFoldersSql: add: " + fi.FullName + " " + UnixTime(DateTime.UtcNow));
+                    connection.Open();
+                    command.CommandText = "INSERT INTO `test`.`worker` (`name`, `path`, `loc`, `size`, `csum`, `dom`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + fi.Name + "', '" + fi.FullName + "', '" + fi.FullName.Split('\\')[0] + "', '" + fi.Length + "', '" + Datei2SHA(fi.FullName) + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[0] + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + File.GetAccessControl(@fi.FullName).GetGroup(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + UnixTime(DateTime.Now) + "', '" +  UnixTime(fi.LastAccessTime) + "', '" +  UnixTime(fi.CreationTime) + "', '" +  UnixTime(fi.LastWriteTime) + "', '1');";
+                    MySqlDataReader Reader;
+                    Reader = command.ExecuteReader();
+                    counter+=1;
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("walkFoldersSql: " + counter + " Zeilen hinzugefuegt.");
+        }
+
+        public static long UnixTimeNow()
+        {
+            TimeSpan _TimeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)_TimeSpan.TotalSeconds;
+        }
+
+        public static long UnixTime(DateTime filetime)
+        {
+            TimeSpan _TimeSpan = (filetime - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)_TimeSpan.TotalSeconds;
+        }
+
+        
         private static void walkFolders(string Directory)  {
             walkFolders(new DirectoryInfo(Directory));
         }
