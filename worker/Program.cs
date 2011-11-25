2901c8b48a929e3b3e7d3f73b1e5d870302fbd10
marcel <mkirbst@htwm.de>
2011-11-25 15:46:22 +0100

    mssql routines improved

--- worker/Program.cs
+++ worker/Program.cs
@@ -19,10 +19,6 @@ namespace worker  {
     
     
     class Program    {
-        static string myConnectionString = "SERVER=53.100.11.229;" +
-                                                  "DATABASE=worker;" +
-                                                  "UID=root;" +
-                                                  "PASSWORD=cNtN.5db6!;";
         static string path = "Q:\\";
         static string logfilename = "logfile.txt";
         static int gc = 0;
@@ -30,87 +26,25 @@ namespace worker  {
 
         static void Main(string[] args)   {
             //Daten des Sqlserver eintragen, spater per Kommandozeilenparameter/Konfigurationsdatei
-            Sqlcreds sc = new Sqlcreds();
-            sc.setHost("53.100.11.229");
-            sc.setDatabase("workerdb");
-            sc.setTable("testtable");
-            sc.setUsername("dbworker");
-            sc.setPassword("");
+            Sqlcreds mysql_linux = new Sqlcreds();
+            mysql_linux.setHost("53.100.11.229");
+            mysql_linux.setDatabase("workerdb");
+            mysql_linux.setTable("worker");
+            mysql_linux.setUsername("dbworker");
+            mysql_linux.setPassword("Schl8ship");
 
+            
+            
             //FileWrite(logfilename, DateTime.Now+": Starte Dateiindexierung fuer "+path+"\n\n");
             //mysql_trunc_table(sc);        
-            walkFoldersSql(path, sc);
-            //mysql_create_table(sc);
-            //mysql_drop_table(sc);
+            mysql_drop_table(mysql_linux);
+            mysql_create_table(mysql_linux);
+            walkFoldersSql(path, mysql_linux);
             //truncmssql();
-            //parsecuso();
-        }
-
-        
-        private static void walkFoldersSql(string DirectorySql, Sqlcreds sc)
-        {
-            walkFoldersSql(new DirectoryInfo(DirectorySql), sc);
-        }
-
-        private static void walkFoldersSql(DirectoryInfo disql, Sqlcreds sc)
-        {
-            int lc = 0;
-            string sqlcommand = "";
             
-            Console.WriteLine("walkFoldersSql: Versuche Verbindung ...");
-
-            try
-            {
-                
-                
-                // Alle Verzeichnisse rekursiv durchlaufen
-                foreach (DirectoryInfo subdir in disql.GetDirectories())
-                {
-                    walkFoldersSql(subdir, sc);
-                }
-
-                // Alle Dateien des Verzeichnisses durchlaufen
-                foreach (FileInfo fi in disql.GetFiles())
-                {
-                    ++gc;
-                    ++lc;
-                    MySqlConnection connection = new MySqlConnection(sc.getMysqlConStr());
-                    MySqlCommand command = connection.CreateCommand();
-                    
-                    String output = DateTime.Now + ": add Nr." + gc + " [" + hrs(fi.Length) + "/" + hrs(globalsize) + " ges]: " + fi.FullName;
-                    Console.WriteLine(output);
-                    FileAppend(logfilename, output+"\n");
-                    
-                    connection.Open();
-                    command.CommandText = "INSERT INTO `"+sc.getDatabase()+"`.`"+sc.getTable()+"` (`name`, `path`, `loc`, `size`, `csum`, `dom`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + fi.Name + "', '" + fi.FullName + "', '" + fi.FullName.Split('\\')[0] + "', '" + fi.Length + "', '" + Datei2SHA(fi.FullName)/*"--not computed--"*/ + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[0] + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + File.GetAccessControl(@fi.FullName).GetGroup(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + UnixTime(DateTime.Now) + "', '" + UnixTime(fi.LastAccessTime) + "', '" + UnixTime(fi.CreationTime) + "', '" + UnixTime(fi.LastWriteTime) + "', '1');";
-                    MySqlDataReader Reader;
-                    Reader = command.ExecuteReader();
-                    globalsize += fi.Length;
-                    connection.Close();
-                
-                }
-                Console.WriteLine("walkFoldersSql: " + lc + " Zeilen hinzugefuegt.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
         }
+
         
-        public static void FileWrite(String sFilename, String sLines)
-        {
-            StreamWriter myFile = new StreamWriter(sFilename);
-            myFile.Write(sLines);
-            myFile.Close();
-        }
-        
-        public static void FileAppend(string sFilename, string sLines)
-        {
-            StreamWriter myFile = new StreamWriter(sFilename, true);
-            myFile.Write(sLines);
-            myFile.Close();
-        }
-
         /**get unix timeformat
          */ 
         public static long UnixTime(DateTime filetime)
@@ -168,19 +102,19 @@ namespace worker  {
             return hrsize;
         }
 
-        public static bool mysql_insert_row(Sqlcreds sc)
+        public static bool mysql_insert_row(Sqlcreds sc, String filepath)
         {
             bool erg = true;
 
             
             Console.WriteLine(": Versuche Verbindung zu: " + sc.getMysqlConStr());
 
-            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlConnection connection = new MySqlConnection(sc.getMysqlConStr());
             MySqlCommand command = connection.CreateCommand();
 
 
 
-            command.CommandText = "INSERT INTO `test`.`memworker` (`name`, `path`, `loc`, `size`, `csum`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + "" + "', 'c:\\testdateien\\mmmm.pdf', 'c:', '1234', 'mmmmfc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75', 'm', 'm', CURRENT_TIMESTAMP, '1983-10-10 22:11:02', '1983-01-01 00:11:02', '1983-10-10 22:11:11', '1');";
+            command.CommandText = "INSERT INTO `"+sc.getDatabase()+"`.`"+sc.getTable()+"` (`name`, `path`, `loc`, `size`, `csum`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + "" + "', '"+filepath+"', 'c:', '1234', 'mmmmfc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75', 'm', 'm', CURRENT_TIMESTAMP, '1983-10-10 22:11:02', '1983-01-01 00:11:02', '1983-10-10 22:11:11', '1');";
             MySqlDataReader Reader;
             connection.Open();
             Reader = command.ExecuteReader();
@@ -225,7 +159,14 @@ namespace worker  {
             command.CommandText = "DROP TABLE " + sc.getTable() + ";";
             MySqlDataReader Reader;
             connection.Open();
-            Reader = command.ExecuteReader();
+            try {
+                Reader = command.ExecuteReader();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Tabelle konnte nicht geloescht werden: " + sc.getTable());
+            }
             connection.Close();
             Console.WriteLine("mysql_drop_table: Trenne Verbindung...");
             return erg;
@@ -270,7 +211,7 @@ namespace worker  {
             
             Console.WriteLine("sql_dump: Versuche Verbindung zu: " + sc.getMysqlConStr());
 
-            MySqlConnection connection = new MySqlConnection(myConnectionString);
+            MySqlConnection connection = new MySqlConnection(sc.getMysqlConStr());
             MySqlCommand command = connection.CreateCommand();
             command.CommandText = "SELECT * FROM "+sc.getTable()+";";
             MySqlDataReader Reader;
@@ -291,33 +232,37 @@ namespace worker  {
             return erg;
         }
 
-        private static void mssql_insert_row(Sqlcreds sc)
+        /*192.168.1.2 sqluser sqlpassword mydatabase newtable file.csv*/
+        private static void parsecuso(String server, String user, String pass, String db, String table, String file)
         {
+            int ds = 0;
+            string strConString = "Data Source=" + server + ";Initial Catalog= " + db + ";User Id= " + user + ";Password=" + pass + ";";
             try
             {                                                                           /*Encoding vom Betriebssystem holen, fuer Umlaute*/
-                using (StreamReader readFile = new StreamReader("C:\\temp\\Cuso.csv", System.Text.Encoding.Default))
+                using (StreamReader readFile = new StreamReader(file, System.Text.Encoding.Default))
                 {
                     string line;
                     string[] row;
-
-
-
                     while ((line = readFile.ReadLine()) != null)
                     {
-
                         row = line.Split(';');
-
                         if (!(row[0] == "ist Firma"))
                         {
                             ds++;
                             SqlConnection sqlCon = new SqlConnection();
                             sqlCon.ConnectionString = strConString;
                             sqlCon.Open();
-                            string strSqlQuery = "INSERT INTO Cuso_Kunden(istFirma, VIPFlag, Kundennummer, Vorname, Name, Firmenname, Strasse, PLZ, Ort, Teilnehmer) VALUES ('" + row[0].Replace("'", "") + "', '" + row[1].Replace("'", "") + "', '" + row[2].Replace("'", "") + "', '" + row[3].Replace("'", "") + "', '" + row[4].Replace("'", "") + "', '" + row[5].Replace("'", "") + "', '" + row[6].Replace("'", "") + "', '" + row[7].Replace("'", "") + "', '" + row[8].Replace("'", "") + "', '" + row[9].Replace("'", "") + "')";
+                            string strSqlQuery = "INSERT INTO " + table + "(istFirma, VIPFlag, Kundennummer, Vorname, Name, Firmenname, Strasse, PLZ, Ort, Teilnehmer) VALUES ('" + row[0].Replace("'", "") + "', '" + row[1].Replace("'", "") + "', '" + row[2].Replace("'", "") + "', '" + row[3].Replace("'", "") + "', '" + row[4].Replace("'", "") + "', '" + row[5].Replace("'", "") + "', '" + row[6].Replace("'", "") + "', '" + row[7].Replace("'", "") + "', '" + row[8].Replace("'", "") + "', '" + row[9].Replace("'", "") + "')";
 
                             SqlCommand sqlCmd = new SqlCommand(strSqlQuery, sqlCon);
                             int intCheckQuery = sqlCmd.ExecuteNonQuery();
-                            if (intCheckQuery > 0) Console.WriteLine(ds + ": " + row[4] + ", " + row[3]);
+                            if (intCheckQuery > 0)
+                            {
+                                if (!(row[4].Equals("")))
+                                    Console.WriteLine(ds + " Kunde: " + row[4] + ", " + row[3]);
+                                else
+                                    Console.WriteLine(ds + " Firma: " + row[5]);
+                            }
                             else Console.WriteLine("Datensatz konnte nicht hinzugefügt werden");
                             sqlCon.Close();
                         }
@@ -330,7 +275,53 @@ namespace worker  {
                 Console.WriteLine("FEHLER: " + e.Message);
             }
 
-            
+        }
+
+        private static void mssql_create_table(Sqlcreds sc)
+        {
+            SqlConnection sqlCon = new SqlConnection(); // Neue Datenbankverbindung
+            sqlCon.ConnectionString = sc.getMssqlConStr(); // Connectionstring wird sqlConnection zugewiesen
+            sqlCon.Open();  // Verbindung zur Datenbank herstellen
+            // Querystring
+            string strSqlQuery = "CREATE TABLE " + sc.getTable() + "(istFirma nvarchar(256), VIPFlag bit, Kundennummer nvarchar(256), Vorname nvarchar(256), Name nvarchar(256), Firmenname nvarchar(256), Strasse nvarchar(256), PLZ nvarchar(256), Ort nvarchar(256), Teilnehmer nvarchar(1024), PRIMARY KEY (Kundennummer),UNIQUE (Kundennummer))";
+            SqlCommand sqlCmd = new SqlCommand(strSqlQuery, sqlCon); // SQLCommand
+            int intCheckQuery = sqlCmd.ExecuteNonQuery();
+            Console.WriteLine("Tabelle angelegt: " + sc.getTable() + "\n");
+            sqlCon.Close();
+        }
+
+        private static void mssql_dump_table(Sqlcreds sc)
+        {
+            // Neue Datenbankverbindung
+            SqlConnection sqlCon = new SqlConnection();
+            // Connectionstring wird sqlConnection zugewiesen
+            sqlCon.ConnectionString = sc.getMssqlConStr();
+            // Verbindung zur Datenbank herstellen
+            sqlCon.Open();
+            // Querystring
+            string strSqlQuery = "SELECT * FROM " + sc.getTable() + ";";
+            // SQLCommand
+            SqlCommand sqlCmd = new SqlCommand(strSqlQuery, sqlCon);
+            int intCheckQuery = sqlCmd.ExecuteNonQuery();
+            Console.WriteLine("Tabelle ausgelesen: " + sc.getTable());
+            sqlCon.Close();
+        }
+
+        private static void mssql_drop_table(Sqlcreds sc)
+        {
+            // Neue Datenbankverbindung
+            SqlConnection sqlCon = new SqlConnection();
+            // Connectionstring wird sqlConnection zugewiesen
+            sqlCon.ConnectionString = sc.getMssqlConStr();
+            // Verbindung zur Datenbank herstellen
+            sqlCon.Open();
+            // Querystring
+            string strSqlQuery = "DROP TABLE " + sc.getTable() + ";";
+            // SQLCommand
+            SqlCommand sqlCmd = new SqlCommand(strSqlQuery, sqlCon);
+            int intCheckQuery = sqlCmd.ExecuteNonQuery();
+            Console.WriteLine("Tabelle geloescht: " + sc.getTable());
+            sqlCon.Close();
         }
 
         private static void mssql_trunc_table(Sqlcreds sc)
@@ -350,5 +341,71 @@ namespace worker  {
             sqlCon.Close();
         }
 
+
+        private static void walkFoldersSql(string DirectorySql, Sqlcreds sc)
+        {
+            walkFoldersSql(new DirectoryInfo(DirectorySql), sc);
+        }
+
+        private static void walkFoldersSql(DirectoryInfo disql, Sqlcreds sc)
+        {
+            int lc = 0;
+            string sqlcommand = "";
+
+            Console.WriteLine("walkFoldersSql: Versuche Verbindung ...");
+
+            try
+            {
+
+
+                // Alle Verzeichnisse rekursiv durchlaufen
+                foreach (DirectoryInfo subdir in disql.GetDirectories())
+                {
+                    walkFoldersSql(subdir, sc);
+                }
+
+                // Alle Dateien des Verzeichnisses durchlaufen
+                foreach (FileInfo fi in disql.GetFiles())
+                {
+                    ++gc;
+                    ++lc;
+                    MySqlConnection connection = new MySqlConnection(sc.getMysqlConStr());
+                    MySqlCommand command = connection.CreateCommand();
+
+                    String output = DateTime.Now + ": add Nr." + gc + " [" + hrs(fi.Length) + "/" + hrs(globalsize) + " ges]: " + fi.FullName;
+                    Console.WriteLine(output);
+                    FileAppend(logfilename, output + "\n");
+
+                    connection.Open();
+                    command.CommandText = "INSERT INTO `" + sc.getDatabase() + "`.`" + sc.getTable() + "` (`name`, `path`, `loc`, `size`, `csum`, `dom`, `owner`, `group`, `stime`, `atime`, `ctime`, `mtime`, `dups`) VALUES ('" + fi.Name + "', '" + fi.FullName + "', '" + fi.FullName.Split('\\')[0] + "', '" + fi.Length + "', '" + Datei2SHA(fi.FullName)/*"--not computed--"*/ + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[0] + "', '" + File.GetAccessControl(@fi.FullName).GetOwner(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + File.GetAccessControl(@fi.FullName).GetGroup(typeof(NTAccount)).ToString().Split('\\')[1] + "', '" + UnixTime(DateTime.Now) + "', '" + UnixTime(fi.LastAccessTime) + "', '" + UnixTime(fi.CreationTime) + "', '" + UnixTime(fi.LastWriteTime) + "', '1');";
+                    MySqlDataReader Reader;
+                    Reader = command.ExecuteReader();
+                    globalsize += fi.Length;
+                    connection.Close();
+
+                }
+                Console.WriteLine("walkFoldersSql: " + lc + " Zeilen hinzugefuegt.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void FileWrite(String sFilename, String sLines)
+        {
+            StreamWriter myFile = new StreamWriter(sFilename);
+            myFile.Write(sLines);
+            myFile.Close();
+        }
+
+        public static void FileAppend(string sFilename, string sLines)
+        {
+            StreamWriter myFile = new StreamWriter(sFilename, true);
+            myFile.Write(sLines);
+            myFile.Close();
+        }
+
+    
     }
 }
